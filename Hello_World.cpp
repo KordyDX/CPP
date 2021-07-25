4e4614b87d2acbd21f7b36949d86f1095bf48cff
Kristián Stefurak <76947058+KordyDX@users.noreply.github.com>
2021-07-25 23:32:18 +0200

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char kokot[150];
+    cin >> kokot;
+    cout << kokot;
+}
---
+++ solotask.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//Sololearn course task
+int main() {
+    int t, z;
+    cin >> t;
+    z = t % 50;
+    cout << 50 - z;
+    
+    return 0;
+}
